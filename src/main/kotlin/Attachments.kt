f73016d96100dc54d70f1540c1ec212e63567ba5
Volostnov Maksim <volostnov@tele2med.ru>
2023-05-28 20:19:49 +0300

    Задача №2. Attachments (доработка)

--- src/main/kotlin/Attachments.kt
+++ src/main/kotlin/Attachments.kt
@@ -2,47 +2,82 @@ interface Attachments {
     val type: String
 }
 
-class Photo(
-    val id: Int,
-    val ownerId: Int,
-    val photo130: String,
+interface AudioAtttachment: Attachments {
+    val id: Int
+    val ownerId: Int
+    val artist: String
+    val title: String
+}
+
+interface PhotoAttachment: Attachments {
+    val id: Int
+    val ownerId: Int
+    val photo130: String
     val photo604: String
-) : Attachments {
+}
+
+interface VideoAttachment: Attachments {
+    val id: Int
+    val ownerId: Int
+    val title: String
+    val duration: Int
+}
+
+interface DocumentAttachment: Attachments{
+    val id: Int
+    val ownerId: Int
+    val title: String
+    val size: Int
+}
+
+interface GraffitiAttachment: Attachments {
+    val id: Int
+    val ownerId: Int
+    val photo130: String
+    val photo604: String
+}
+
+class Photo(
+    override val id: Int,
+    override val ownerId: Int,
+    override val photo130: String,
+    override val photo604: String
+) : PhotoAttachment {
     override val type: String = "photo"
 }
 
 class Video(
-    val id: Int,
-    val ownerId: Int,
-    val title: String,
-    val duration: Int
-) : Attachments {
+    override val id: Int,
+    override val ownerId: Int,
+    override val title: String,
+    override val duration: Int
+) : VideoAttachment {
     override val type: String = "video"
 }
 
 class Audio(
-    val id: Int,
-    val ownerId: Int,
-    val artist: String,
-    val title: String
-) : Attachments {
+    override val id: Int,
+    override val ownerId: Int,
+    override val artist: String,
+    override val title: String
+) : AudioAtttachment {
     override val type: String = "audio"
 }
 
 class Document(
-    val id: Int,
-    val ownerId: Int,
-    val title: String,
-    val size: Int
-) : Attachments {
+    override val id: Int,
+    override val ownerId: Int,
+    override val title: String,
+    override val size: Int
+) : DocumentAttachment {
     override val type: String = "document"
 }
 
 class Graffiti(
-    val id: Int,
-    val ownerId: Int,
-    val photo130: String,
-    val photo604: String
-) : Attachments {
+    override val id: Int,
+    override val ownerId: Int,
+    override val photo130: String,
+    override val photo604: String
+) : GraffitiAttachment {
     override val type: String = "graffiti"
 }
